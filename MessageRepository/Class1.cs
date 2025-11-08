ef8afa2d2e895c1360567f19d1d631f01b55623a
Metehan Öztürk <yalnizkodlamacii@gmail.com>
2025-11-09 00:00:24 +0300

    Add files via upload

---
+++ MessageRepository/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace MessageRepository
+{
+    public class MessageRepo
+    {
+
+        private List<string> Message = new List<string>();
+        public readonly string path = Path.Combine(AppContext.BaseDirectory ,"../" , "../" , "../" , "../", "MessageRepository/" , "message.json");
+        public void Add(string message) 
+        {
+            Message.Add(message);
+        }
+
+        public void Save() 
+        {
+            var fullpath = Path.GetFullPath(path);
+
+            var formatted = Message.Select(m => new { Message = m }).ToList();
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsondata = JsonSerializer.Serialize(formatted, options);
+
+            File.WriteAllText(fullpath, jsondata);
+        }
+        
+
+
+    }
+}
